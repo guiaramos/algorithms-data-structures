44bcbdfb1a352a52afe81d8ef1463725c7007121
Guilherme Ramos <gui_aramos@outlook.com>
2020-12-22 21:53:51 +0900

    :star: add(pattern): add frequency counter pattern

---
+++ javascript/src/patterns/frequency_counter.test.ts
@@ -0,0 +1,39 @@
+import { same } from './frequency_counter'
+
+describe('Frequency Counter Pattern', () => {
+  it('should return false if length of the arrays are different', () => {
+    const firstArray: number[] = [1, 2, 4, 5]
+    const secondArray: number[] = [1, 2, 4, 5, 6]
+
+    const isSame = same(firstArray, secondArray)
+
+    expect(isSame).toBeFalsy()
+  })
+
+  it('should return false if number is not square', () => {
+    const firstArray: number[] = [1, 2, 3]
+    const secondArray: number[] = [1, 4, 6]
+
+    const isSame = same(firstArray, secondArray)
+
+    expect(isSame).toBeFalsy()
+  })
+
+  it('should return true for ordered array', () => {
+    const firstArray: number[] = [1, 2, 3]
+    const secondArray: number[] = [1, 4, 9]
+
+    const isSame = same(firstArray, secondArray)
+
+    expect(isSame).toBeTruthy()
+  })
+
+  it('should return true for unordered array', () => {
+    const firstArray: number[] = [2, 3, 1]
+    const secondArray: number[] = [9, 1, 4]
+
+    const isSame = same(firstArray, secondArray)
+
+    expect(isSame).toBeTruthy()
+  })
+})
