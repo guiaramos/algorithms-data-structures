aa1b83ea398d0899068bbe5db49dc2bbf975c1ac
Guilherme Ramos <gui_aramos@outlook.com>
2020-12-23 22:16:39 +0900

    :star: add(pattern): add frequency counter pattern

--- python/patterns/frequency_counter.py
+++ python/patterns/frequency_counter.py
@@ -6,9 +6,31 @@ def sameArraySquared(arrOne, arrTwo):
         return False
 
     # Make a dict for each arrays to save the frequency
+    freqArrayOne = {}
+    freqArrayTwo = {}
+
     # Interate thru the first array and save the frequency
+    for key in arrOne:
+        if key in freqArrayOne:
+            freqArrayOne[key] += 1
+        else:
+            freqArrayOne[key] = 1
+
     # Interate thru the second array and save the frequency
+    for key in arrTwo:
+        if key in freqArrayTwo:
+            freqArrayTwo[key] += 1
+        else:
+            freqArrayTwo[key] = 1
+
     # Interate thru the first frequency dict
-    # Check if the key squared is on second array frequency
-    # Check if the value is same
+    for key in freqArrayOne:
+        # Check if the key squared is on second array frequency
+        if key ** 2 not in freqArrayTwo:
+            return False
+
+            # Check if the value is same
+        if freqArrayTwo[key ** 2] != freqArrayOne[key]:
+            return False
+
     return True
--- python/patterns/frequency_counter_test.py
+++ python/patterns/frequency_counter_test.py
@@ -3,7 +3,7 @@ from frequency_counter import sameArraySquared
 
 
 class TestSameArraySquared(unittest.TestCase):
-    def test_array(self):
+    def test_wrong_length(self):
         # Test if arrays have the dif length should return false
         firstArray = [1, 2, 3]
         secondArray = [1, 2, 3, 4]
@@ -11,10 +11,34 @@ class TestSameArraySquared(unittest.TestCase):
         isSame = sameArraySquared(firstArray, secondArray)
         self.assertFalse(isSame)
 
-    def test_array_with_no_squared(self):
+    def test_no_squared(self):
         # Test if the array with no correct squared returns false
-        firstArray = [1, 2, 3]
-        secondArray = [1, 2, 3]
+        firstArray = [1, 3, 3]
+        secondArray = [1, 2, 2]
 
         isSame = sameArraySquared(firstArray, secondArray)
         self.assertFalse(isSame)
+
+    def test_wrong_squared(self):
+        # Test if the array has same frequency but wrong squared
+        firstArray = [1, 2, 1]
+        secondArray = [1, 4, 6]
+
+        isSame = sameArraySquared(firstArray, secondArray)
+        self.assertFalse(isSame)
+
+    def test_correct_squared_ordered(self):
+        # Test with correct array and squared - ordered
+        firstArray = [3, 4, 5]
+        secondArray = [9, 16, 25]
+
+        isSame = sameArraySquared(firstArray, secondArray)
+        self.assertTrue(isSame)
+
+    def test_correct_squared_unordered(self):
+        # Test with correct array and squared - unordered
+        firstArray = [4, 5, 3]
+        secondArray = [16, 25, 9]
+
+        isSame = sameArraySquared(firstArray, secondArray)
+        self.assertTrue(isSame)
